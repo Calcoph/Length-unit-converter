fd4193c2bd7bee71be0596cb1fe1f07ea02875a8
calcopiritus <diego22222@hotmail.es>
2018-06-05 20:02:27 +0200

    add GUI

--- cu.py
+++ cu.py
@@ -1,113 +1,196 @@
-# 1 inch = 0,0254m
-# 1 foot = 0,3048m // 12in
-# 1 yard = 0,9144m // 3ft
-# 1 mile = 1609,344m // 1760yd
+#cd D:\Diego\Documents\GitHub\Length-unit-converter
 
-# list of available units in a handful of possible spelling
-unitnames = [
-    ["km", "kilometre", "kilometres", "kilometer", "kilometers"],
-    ["hm", "hectometre", "hectometres", "hectometer", "hectometers"],
-    ["dam", "decametre", "decametres", "decameter", "decameters"],
-    ["m", "metre", "metres", "meter", "meters"],
-    ["dm", "decimetre", "decimetres", "decimeter", "decimeters"],
-    ["cm", "centimetre", "centimetres", "centimeter", "centimeters"],
-    ["mm", "milimetre", "milimetres", "milimeter", "milimeters", "millimeter", "millimeters", "millimetre", "millimetres"],
-    ["in", "inch", "inches"], ["ft", "foot", "feet"], ["yd", "yard", "yards"],
-    ["mi", "mile", "miles", "mille", "milles"]
-    ["leag", "league", "leagues", "leage", "leages"]
-    ["er", "earth radius", "earth radiuses", "earth radious", "earth radiouses"]
-    ["ld", "lunar distance", "lunar distances", "moon distance", "moon distances"]
-    ["au", "astronomical unit", "astronomical units", "astro unit", "astro units"]
-    ["ly", "light year", "light years", "ligth year", "ligth years"]
-    ["pc", "parsec", "parsecs", "parasec", "parasecs", "parasect", "parasects"]
-    ["smt", "smoot", "smoots", "moot", "moots"]
-]
-# this list is used to know which uintnames list is for what unit
-units = {
-    0: "km", 1: "hm", 2: "dam", 3: "m", 4: "dm", 5: "cm", 6: "mm",
-    7: "inch", 8: "ft", 9: "yd", 10: "mi", 11: "leag", 12: "er", 13: "ld",
-    14: "au", 15: "ly", 16: "pc", 17: "smt"
-}
-# all units converted to meters
-unitsinmetres = {
-    "km": 1000.0, "hm": 100.0, "dam": 10.0, "m": 1.0, "dm": 0.1, "cm": 0.01, "mm": 0.001,
-    "inch": 0.0254, "ft": 0.3048, "yd": 0.9144, "mi": 1609.344, "leag": 4828.032
-    "er": 6371000, "ld": 384402000, "au": 149597870700, "ly": 9460730472580800,
-    "pc": 30856775814671900, "smt": 1.70
-}
+from tkinter import *
+from tkinter import ttk
 
-def unit_finder(inp):
-        out = inp
-        for x in range(len(unitnames)):
-            if inp in unitnames[x]:
-                out = x
-        return out
-def unit_printer():
-    units2 = []
-    for x in range(len(unitnames)):
-        units2.append((unitnames[x][1]))
-    ", ".join(units2)
-    return units2
-def unit_checker(inp):
-    isin = False
-    for x in range(len(unitnames)):
-        if inp in unitnames[x]:
-            isin = True
-    return isin
-def convert(finalunit, quantity, startingunit):
-    meters = unitsinmetres[units[startingunit]]
-    totalmeters = meters * quantity
-    total = totalmeters / unitsinmetres[units[finalunit]]
-    finalunit = units[finalunit]
-    final = str(total) + finalunit
-    return final
 
-print "***************************************************************************"
-print "\nWelcome to the unit converter! all the available units at the moment are:"
-print unit_printer()
-print
-print "Input a blank answer to restart or 'exit' to end the program \n"
-print "***************************************************************************"
+root = Tk() # La ventana en si
+root.title("Feet to Meters") # Titulo de la ventana
+mainframe = ttk.Frame(root, padding="3 3 12 12") # La ventana de la ventana
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S)) # Colocar la ventana
+mainframe.columnconfigure(0, weight=1)
+mainframe.rowconfigure(0, weight=1)
+# The menu
+menubar = Menu(root)
+filemenu = Menu(menubar, tearoff=0)
+inputname = IntVar()
+outputname = IntVar()
+inputname.set(1)
+outputname.set(0)
+unitnameoptions = Menu(filemenu, tearoff=0)
+unitnameoptions.add_radiobutton(label="Abreviated inputs", variable=inputname, value=0)
+unitnameoptions.add_radiobutton(label="Full named inputs", variable=inputname, value=1)
+unitnameoptions.add_separator()
+unitnameoptions.add_radiobutton(label="Abreviated outputs", variable=outputname, value=0)
+unitnameoptions.add_radiobutton(label="Full named outputs", variable=outputname, value=1)
+filemenu.add_cascade(label="Unit name options", menu=unitnameoptions)
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=root.quit)
+menubar.add_cascade(label="Settings", menu=filemenu)
+root.config(menu=menubar)
 
-will_to_live = True
-while will_to_live == True:
-    fromunitinput = raw_input("convert from ").lower()
-    if fromunitinput == "exit":
-        will_to_live = False
-        break
-    elif fromunitinput == "":
-        print
-    else:
-        if unit_checker(fromunitinput):
-            fromunit = unit_finder(fromunitinput)
-            quantity = raw_input("How many %s? " % unitnames[fromunit][2])
-            if quantity == "exit":
-                will_to_live = False
+changetheinputs = lambda:changeinputs(unitinputbox, unitoutputbox)
+input = IntVar()
+output = StringVar()
+
+def round(floatnumber):
+    stringnumber = str(float(floatnumber))
+    if "." in stringnumber:
+        indexofdot = stringnumber.index(".")
+        newnumber = []
+        iterations = 0
+        while iterations <= (indexofdot + 2):
+            if (iterations + 1) > len(stringnumber):
                 break
-            elif quantity == "":
-                print
-            else:
-                if quantity.isdigit():
-                    quantity = int(quantity)
-                    tounitinput = raw_input("convert to ").lower()
-                    tounit = unit_finder(tounitinput)
-                    if tounitinput == "exit":
-                        will_to_live = False
-                        break
-                    elif tounitinput == "":
-                        print
+            if iterations == (indexofdot + 2):
+                try:
+                    if int(stringnumber[indexofdot + 3]) >= 5:
+                        newnumber.append(str(int(stringnumber[indexofdot + 2]) + 1))
                     else:
-                        if unit_checker(tounitinput):
-                            print convert(tounit, quantity, fromunit)
-                        else:
-                            print
-                            print "That is not a valid unit"
-                            print
-                else:
-                    print
-                    print "That is not a valid unit"
-                    print
+                        newnumber.append(stringnumber[iterations])
+                except IndexError:
+                    newnumber.append(stringnumber[iterations])
+            else:
+                newnumber.append(stringnumber[iterations])
+            iterations += 1
+        if "e" in stringnumber:
+            indexofe = stringnumber.index("e")
+            iterations = indexofe
+            while iterations < len(stringnumber):
+                newnumber.append(stringnumber[(iterations)])
+                iterations += 1
+            newnumber = "".join(newnumber)
+            return newnumber
         else:
-            print
-            print "That is not a valid unit"
-            print
+            newnumber = "".join(newnumber)
+            return newnumber
+    else:
+        return stringnumber
+def convert(finalunit, quantity, startingunit):
+    global output
+    meters = unitsinmeters[startingunit]
+    totalmeters = meters * int(str(quantity.get()))
+    total = totalmeters / unitsinmeters[finalunit]
+    total = round(total)
+    final = total + " " + finalunit
+    output.set(final)
+def changeinputs(unitinputbox, unitoutputbox):
+    inputbefore = unitinputbox.get()
+    outputbefore = unitoutputbox.get()
+    unitinputbox.set(outputbefore)
+    unitoutputbox.set(inputbefore)
+
+# all units converted to meters
+unitsinmeters = {
+    "kilometer": 1000.0,
+    "hectometer": 100.0,
+    "decameter": 10.0,
+    "meter": 1.0,
+    "decimeter": 0.1,
+    "centimeter": 0.01,
+    "millimeter": 0.001,
+    "inch": 0.0254,
+    "foot": 0.3048,
+    "yard": 0.9144,
+    "mile": 1609.344,
+    "league": 4828.032,
+    "earth radius": 6371000.0,
+    "lunar distance": 384402000.0,
+    "astronomical unit": 149597870700.0,
+    "light year": 9460730472580800.0,
+    "parsec": 30856775814671900.0,
+    "smoot": 1.70
+    }
+# List of all available units (abreviation, name, plural name)
+unitnames = [
+    ["km", "kilometer", "kilometers"],
+    ["hm", "hectometer", "hectometers"],
+    ["dam","decameter", "decameters"],
+    ["m", "meter", "meters"],
+    ["dm", "decimeter", "decimeters"],
+    ["cm", "centimeter", "centimeters"],
+    ["mm", "millimeter", "millimeters"],
+    ["in", "inch", "inches"],
+    ["ft", "foot", "feet"],
+    ["yd", "yard", "yards"],
+    ["mi", "mile", "miles"],
+    ["leag", "league", "leagues"],
+    ["er", "earth radius", "earth radiuses"],
+    ["ld", "lunar distance", "lunar distances"],
+    ["au", "astronomical unit", "astronomical units"],
+    ["ly", "light year", "light years"],
+    ["pc", "parsec", "parsecs"],
+    ["smt", "smoot", "smoots"]
+    ]
+
+def getinputname():# !!!!!! NECESITA TRABAJO AQUI!!!!!!
+    if inputname.get() == 0:
+        values = [
+        "km",
+        "hm",
+        "dam",
+        "m",
+        "dm",
+        "cm",
+        "mm",
+        "in",
+        "ft",
+        "yd",
+        "mi",
+        "leag",
+        "er",
+        "ld",
+        "au",
+        "ly",
+        "pc",
+        "smt"
+        ]
+    elif inputname.get() == 1:
+        values = [
+        "kilometer",
+        "hectometer",
+        "decameter",
+        "meter",
+        "decimeter",
+        "centimeter",
+        "millimeter",
+        "inch",
+        "foot",
+        "yard",
+        "mile",
+        "league",
+        "earth radius",
+        "lunar distance",
+        "astronomical unit",
+        "light year",
+        "parsec",
+        "smoot"
+        ]
+    return values
+
+# Most of the widgets
+ttk.Label(mainframe, text="Equals to:").grid(column=1, row=3, sticky=(N, S, W, E))
+ttk.Label(mainframe, text="Convert any length unit to another one instantly!").grid(column=1, row=1, columnspan=2, sticky=(N, S, W, E))
+ttk.Label(mainframe, textvariable=output).grid(column=1, row=4, sticky=(N, S, W, E))
+ttk.Button(mainframe, text="^v", command=changetheinputs).grid(column=2, row=3, sticky=(N, S, W, E))
+ttk.Button(mainframe, text="CONVERT", command=lambda:convert(unitoutputbox.get(), input, unitinputbox.get())).grid(column=1, row=5, columnspan=2, padx=100, pady=(12, 0), sticky=(N, S, W, E))
+unitinputbox = ttk.Combobox(mainframe, state="readonly", values=getinputname())
+unitinputbox.grid(column=2, row=2, sticky=(N, S, W, E))
+unitoutputbox = ttk.Combobox(mainframe, state="readonly", values=getinputname())
+unitoutputbox.grid(column=2, row=4, sticky=(N, S, W, E))
+unitinputbox.set("meter")
+unitoutputbox.set("foot")
+quantityinput = ttk.Entry(mainframe, textvariable=input)
+quantityinput.grid(column=1, row=2, sticky=(N, S, W, E))
+
+
+# Bindings
+root.bind('<Return>', lambda event:convert(unitoutputbox.get(), input, unitinputbox.get()))
+# Configure the entry
+input.set("")
+quantityinput.focus()
+# Set the minnimum window size
+root.update()
+root.minsize(root.winfo_width(), root.winfo_height())
+mainframe.pack(expand=1)
+root.mainloop()
